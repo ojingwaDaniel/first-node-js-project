f463c6f1b4c765687356cd9fb46fb94b4d25c720
ojingwaDaniel <ojingwadanny@gmail.com>
2023-03-03 20:53:11 +0100

    first commit

--- filetransfer.js
+++ filetransfer.js
@@ -1,2 +1,7 @@
 const {list,ages} = require('./people')
 console.log(list,ages)
+// Opreating system
+const os = require('os')
+// system brand and homne directory
+console.log(os.platform(),os.homedir())
+// file system
