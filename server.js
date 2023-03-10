124b26fa8b574e87638775ec5dabdbe78510850c
ojingwaDaniel <ojingwadanny@gmail.com>
2023-03-10 20:36:37 +0100

    added a server containing a request and a response

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const http = require('http')
 const server = http.createServer((request,response)=>{
-    console.log('request made')
+    console.log(request.url,request.method)
+    // set header
+    response.setHeader('content-type','text/plain')
+    response.write('You are doing a great job you will be fine')
+    response.end()
 })
 server.listen(3000,'localhost',()=>{
     console.log('listening from the port number 3000')
