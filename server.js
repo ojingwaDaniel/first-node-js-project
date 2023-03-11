af3491a77a25093c12bbfc5f0878c2022a13ce3f
ojingwaDaniel <ojingwadanny@gmail.com>
2023-03-11 11:25:33 +0100

    added a server containing a request and a response

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs')
 const server = http.createServer((request,response)=>{
     console.log(request.url,request.method)
     // set header
-    response.setHeader('content-type','text/plain')
-    response.write('You are doing a great job you will be fine')
-    response.end()
+    response.setHeader('content-type','text/html')
+    let path = './views/'
+    switch(request.url){
+        case '/':
+            path += 'index.html'
+            break
+        case '/about':
+            path += 'about.html'
+            break
+        default:
+            path += '404.html'        
+    }
+    fs.readFile(path,(err,data)=>{
+        if (err) {
+            console.log(err)
+            response.end()
+            
+        } else {
+            // response.write(data)
+            response.end(data)
+            
+        }
+    })
+    
 })
 server.listen(3000,'localhost',()=>{
     console.log('listening from the port number 3000')
