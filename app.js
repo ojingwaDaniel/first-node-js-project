eb9b56017e4fa3a25094e9787c12125f0f09a4a4
ojingwaDaniel <ojingwadanny@gmail.com>
2023-03-13 22:10:34 +0100

    added a content from the app.js to the index ejs

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express')
 // express app
 const app = express()
+// registering ejs
+app.set('view engine','ejs')
 // listening to request
 app.listen(3000)
 // getting response
 app.get("/", (request, response) => {
-  response.sendFile('./views/index.html',{root :__dirname});
+  const blogs = [
+    {title : 'Definition of success',snippet : 'Giving the required result of a task with persistence and consistency'},
+    {title : 'Salvation',snippet : 'Accepting Jesus as your personal saviour and receiving a new life'},
+    {title : 'Love',snippet : 'Love endures everything and remembers no wrong ,put others before oneself'},
+    {title : 'Association',snippet : 'Close Friendship you have among people which should be  done intentionally'}
+  ]
+  response.render('index',{title : 'Home',blogs})
 });
 app.get("/about", (request, response) => {
-  response.sendFile("./views/about.html", { root:__dirname });
+    response.render("about",{title : 'About'});
 });
-// Redirect
-app.get('/about-us',(req,res)=>{
-    res.redirect('/about')
+// create blog
+app.get('/blogs/create',(req,res)=>{
+  res.render('create',{title : 'Create A New Blog'})
 })
 // 404 error
 app.use( (request, response) => {
-  response.status(404).sendFile("./views/404.html", { root:__dirname });
+  response.status(404).render('404',{title : '404'})
 });
