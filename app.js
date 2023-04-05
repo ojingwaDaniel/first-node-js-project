e0b7174b66c9b1951c2bbce03618ffc5d79696c7
ojingwaDaniel <ojingwadanny@gmail.com>
2023-04-05 22:25:06 +0100

    added a content from the app.js to the index ejs

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use((req, res, next) => {
   console.log("Method :", req.method);
   console.log("Method :", req.method);
   console.log("Method :", req.method);
-  console.log("Method :", req.method);
   next();
 });
 
